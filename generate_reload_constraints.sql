7adfc1ede3d18d7149b51b1b81c77da1dde1f372
Razafindrabe <razafindrabem2@gmail.com>
2016-04-20 15:41:22 +0300

    Update and rename creating_constraints.sql to generate_reload_constraints.sql

--- generate_reload_constraints.sql
+++ generate_reload_constraints.sql
@@ -6,3 +6,6 @@ SELECT 'ALTER TABLE "'||nspname||'"."'||relname||'" ADD CONSTRAINT "'||conname||
  INNER JOIN pg_class ON conrelid=pg_class.oid
  INNER JOIN pg_namespace ON pg_namespace.oid=pg_class.relnamespace
  ORDER BY CASE WHEN contype='f' THEN 0 ELSE 1 END DESC,contype DESC,nspname DESC,relname DESC,conname DESC;
+
+
+--enjoy ;)
